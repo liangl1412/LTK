a3d28a6f6701709185c7ea6821df2a7b381c5739
liangl1412 <yi.zhou@xplusz.com>
2015-07-17 17:28:58 -0700

    add more style

--- app/javascripts/src/heroCard.jsx
+++ app/javascripts/src/heroCard.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+
 class HeroCard extends React.Component {
 
 	render() {
 		var commentNodes = this.props.data.map((comment) => {
+		  var imgUrl = "assets/img/hero/" + comment.img + ".jpg";
+
 	      return (
 	        // `key` is a React-specific concept and is not mandatory for the
 	        // purpose of this tutorial. if you're curious, see more here:
@@ -10,13 +13,13 @@ class HeroCard extends React.Component {
 
 	        <div className="grid-12">
 				<div className="card-box">
-					<img src="assets/img/1.png" />
+					<img src={imgUrl} />
 					<div className="card-info">
 						<h3>{comment.name}</h3>
 						{comment.skills.map(function(object, i){
 					        return <p className="card-skills">
 						        <span className="skill-title">{object.title}</span>
-						        <span>{object.desc}</span>
+						        <span dangerouslySetInnerHTML = {{__html: object.desc}} />
 					        </p>
 					    })}
 					</div>
